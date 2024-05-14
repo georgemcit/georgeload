7863812eb98289ebcbfedd468650d3ccccdebce1
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-13 20:34:31 -0400

    Update output.tf

--- loadbalancer/output.tf
+++ loadbalancer/output.tf
@@ -1,1 +1,13 @@
-
+output "loadbalancerconfig" {
+  value = local.loadbalancerconfig
+}
+output "loadbalancerconfig" {
+  value=[for app in local.loadbalancerconfig : app.name]
+}
+output "loadbalancer" {
+  value = var.loadbalancer
+}
+variable "loadbalancer"{
+ type=string
+ default="loadbalancer"
+}
