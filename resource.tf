27f4c6d4670b32948de8cb8575ede33b96769033
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 22:18:59 -0400

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,39 @@
+resource "azurerm_kubernetes_cluster" "george1" {
+  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = var.default_node_pool_name
+    node_count = var.default_node_pool__node_count
+    vm_size    = var.default_node_pool_vm_size 
+  }
+
+  identity {
+    type = var.identity
+  }
+
+  tags = {
+    Environment = var.environment_tag
+  }
+}
+variable "dns_prefix"{
+  type=string
+}
+variable  "default_node_pool_name"{
+  type=string
+}
+variable  "default_node_pool__node_count"{
+  type=number
+}
+variable  "identity"{
+  type=string
+}
+variable  "environment_tag"{
+  type=string
+}
+variable  "default_node_pool_vm_size"{
+  type=string
+}
