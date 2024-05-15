2312452b347ecd9508f8d57eab940fa1aad349c0
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 20:41:20 -0400

    Delete loadbalancer/output.tf

--- loadbalancer/output.tf
+++
@@ -1,9 +0,0 @@
-output "loadbalancerconfig" {
-  value = local.loadbalancerlist
-}
-output "allocation_method" {
-  value=[for app in local.loadbalancerconfig : app.allocation_method]
-}
-output "loadbalancer" {
-  value = var.loadbalancer
-}
