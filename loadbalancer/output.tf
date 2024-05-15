8959023d844994c7af48b46ccb5229a97a7d2689
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 20:49:03 -0400

    Update output.tf

--- loadbalancer/output.tf
+++ loadbalancer/output.tf
@@ -1,1 +1,8 @@
-
+output "loadbalancerconfig" {
+  value = local.loadbalancerlist
+}
+output "allocation_method" {
+  value=[for app in local.loadbalancerconfig : app.allocation_method]
+}
+output "loadbalancer" {
+  value = var.loadbalancer
