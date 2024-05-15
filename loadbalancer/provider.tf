cfbbf6075dd73114d42493d60585599c58819627
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 20:58:41 -0400

    Update provider.tf

--- loadbalancer/provider.tf
+++ loadbalancer/provider.tf
@@ -29,23 +29,13 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
-variable "account_tier"{
-  type=string
-  default="Standard"
+
+variable"loadbalancer_name"{
+   type="LoadNalacserRG"
+   default="West Eruope"
 }
-variable "access_tier"{
-  type=string
-  default="Cool"
-}
-variable "account_replication_type"{
-  type=string
-  default="GRS"
-}
-variable "cross_tenant_replication_enabled"{
- type=bool
- default=false
-}
-variable "environment"{
- type=string
- default="staging"
+
+variable "folser"{
+   type=string
+   default="lbfolder
 }
