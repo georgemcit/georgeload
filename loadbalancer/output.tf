016ad5fc33d2a8e1d6ca2eac84f0ce95c53df492
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 21:21:47 -0400

    Update output.tf

--- loadbalancer/output.tf
+++ loadbalancer/output.tf
@@ -1,1 +1,26 @@
 
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+
+variable"loadbalancer_name"{
+  type=string
+  default="LoadbalanserRG"
+}
+variable"loadbalancer_location"{
+  type=string
+  default="West Europe"
+}
+variable "folder"{
+  type=string
+  default="lbfolder"
+}
