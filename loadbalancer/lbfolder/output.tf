11cebc61ccc734a551cb929265697832a27da7af
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-14 20:41:11 -0400

    Update output.tf

--- loadbalancer/lbfolder/output.tf
+++ loadbalancer/lbfolder/output.tf
@@ -1,1 +1,9 @@
-
+output "loadbalancerconfig" {
+  value = local.loadbalancerlist
+}
+output "allocation_method" {
+  value=[for app in local.loadbalancerconfig : app.allocation_method]
+}
+output "loadbalancer" {
+  value = var.loadbalancer
+}
